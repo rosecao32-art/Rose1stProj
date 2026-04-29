c5529ecabf64cfec6dd419de1557e086533861f8
Rose Cao <rosecao32@gmail.com>
2026-04-28 19:40:58 -0600

    Add text.lower

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,15 @@ def main():
     print("Hello World!!! It is great.")
 
 main()
+
+text = "HELLO Word 123!"
+result = text.lower()
+print(result)
+
+p1 = "AbC123"
+p2 = "abc123"
+print(p1.lower() == p2.lower())
+
+txt = "Machine Learning with PYTHON"
+keyword = "python"
+print(keyword.lower() in txt.lower())
