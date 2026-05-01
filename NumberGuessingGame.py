1167da7ef49f90bec1fd85222f4c24aa114f5649
Rose Cao <rosecao32@gmail.com>
2026-05-01 16:31:00 -0600

    Number Guessing Game

---
+++ NumberGuessingGame.py
@@ -0,0 +1,17 @@
+import random
+
+print("Welcome to the Number Guessing Game!")
+number = random.randint(1, 50)
+attempts = 0
+
+while True:
+    guess = int(input("Guess a number between 1 and 50: "))
+    attempts += 1
+
+    if guess < number:
+        print("Too low! Try again.")
+    elif guess > number:
+        print("Too high! Try again.")
+    else:
+        print(f"Correct! You guessed it in {attempts} tries.")
+        break
